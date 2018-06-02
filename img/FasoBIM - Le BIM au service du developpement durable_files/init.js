1a9b53d5a86376c253c2e96fc56601084b159c65
Toussaint <toussaintpahima@gmail.com>
2018-06-02 14:44:20 +0200

    Footer integration and enhancements
    - Add footer
    - Change images
    - Others enhancements

---
+++ img/FasoBIM - Le BIM au service du developpement durable_files/init.js
@@ -0,0 +1,52 @@
+(function ($) {
+  $(function () {
+
+    $('.nav-wrapper ul a').click(function () {
+      $(this).siblings('a').removeClass('active');
+      $(this).addClass('active');
+    });
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel({
+      duration: 2,
+      numVisible: 7,
+      indicators: true,
+      dist: 0,
+      padding: 5
+    });
+    autoplay();
+
+    function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+    }
+
+
+    $('.smoothscroll').init();
+    $('.slider').slider({
+      height: 500,
+    });
+
+    $(".switch").find("input[type=checkbox]").on("change", function () {
+      var status = $(this).prop('checked');
+      if (status) {
+        window.lang.change('en');
+        return false;
+      } else {
+        window.lang.change('fr');
+        return false;
+      }
+    });
+
+    $("#contactbutton").on("click", function () {
+      Email.send("toussaintpahima@gmail.com",
+        "testweb@fasobim.com",
+        "This is a subject",
+        "this is the body", {
+          token: "083c3b59-11b8-4f62-8a60-dc3dabebc7f3"
+        });
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
